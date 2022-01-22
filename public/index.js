7b690b0bd0343087639ced15a076e41ecdaa1006
guswn2521 <guswn2521@naver.com>
2022-01-22 17:45:03 +0900

    .

--- public/index.js
+++ public/index.js
@@ -1,69 +1,5 @@
 ;
 (function(global, $) {
-    $('.menu > a').click(function(e) {
-        var href = $(this).attr('href')
-
-        var targetTop = $(href).offset().top
-
-        /*
-        // 한번에 가도록 하는 방법
-        $(window).scrollTop(targetTop);
-        */
-
-        $('html').stop().animate({ scrollTop: targetTop }, 300)
-
-        e.preventDefault()
-    })
-
-    function Page__updateIndicatorActive() {
-        var scrollTop = $(window).scrollTop()
-
-        // 역순으로 검색해야 편하다
-        $($('.page').get().reverse()).each(function(index, node) {
-            var $node = $(this)
-            var offsetTop = parseInt($node.attr('data-offset-top'))
-
-            if (scrollTop >= offsetTop) {
-                // 기존 녀석에게 활성화 풀고
-                $('.page-indicator > ul > li.active').removeClass('active')
-                    // 해당하는 녀석에게 활성화 넣고
-
-                var currentPageIndex = $node.index()
-                $('.page-indicator > ul > li').eq(currentPageIndex).addClass('active')
-
-                $('html').attr('data-current-page-index', currentPageIndex)
-
-                return false // 더 이상 다른 페이지를 검사하지 않는다.
-            }
-        })
-    }
-
-    // 각 페이지의 offsetTop 속성을 업데이트
-    function Page__updateOffsetTop() {
-        $('.page').each(function(index, node) {
-            var $page = $(node)
-            var offsetTop = $page.offset().top
-
-            $page.attr('data-offset-top', offsetTop)
-        })
-
-        // 계산이 바뀌었으니까, 다시 상태 업데이트
-        Page__updateIndicatorActive()
-    }
-
-    function Page__init() {
-        Page__updateOffsetTop()
-    }
-
-    // 초기화
-    Page__init()
-
-    // 화면이 리사이즈 할 때 마다, offsetTop을 다시계산
-    $(window).resize(Page__updateOffsetTop)
-
-    // 스크롤이 될 때 마다, 인디케이터의 상태를 갱신
-    $(window).scroll(Page__updateIndicatorActive)
-
     // Go to the TOP
     var btnTop = $('.btn_top')
     btnTop.on('click', 'a', function(e) {
@@ -71,3 +7,44 @@
         $doc.stop().animate({ scrollTop: 0 }, 800)
     })
 })(window, window.jQuery)
+
+// Select all links with hashes
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') ==
+            this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash)
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+                // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault()
+                $('html, body').animate({
+                        scrollTop: target.offset().top,
+                    },
+                    1000,
+                    function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target)
+                        $target.focus()
+                        if ($target.is(':focus')) {
+                            // Checking if the target was focused
+                            return false
+                        } else {
+                            $target.attr('tabindex', '-1') // Adding tabindex for elements not focusable
+                            $target.focus() // Set focus again
+                        }
+                    },
+                )
+            }
+        }
+    })
