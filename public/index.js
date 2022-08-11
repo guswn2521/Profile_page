74a8f44862874a1fd93604d466651e32c4f57cd1
guswn2521@gmail.com <guswn2521@gmail.com>
2022-08-11 15:51:31 +0900

    div 에 건 링크 새창에서 열기

--- public/index.js
+++ public/index.js
@@ -1,50 +1,50 @@
-;
-(function(global, $) {
-    // Go to the TOP
-    var btnTop = $('.btn_top')
-    btnTop.on('click', 'a', function(e) {
-        e.preventDefault()
-        $doc.stop().animate({ scrollTop: 0 }, 800)
-    })
-})(window, window.jQuery)
+(function (global, $) {
+  // Go to the TOP
+  var btnTop = $(".btn_top");
+  btnTop.on("click", "a", function (e) {
+    e.preventDefault();
+    $doc.stop().animate({ scrollTop: 0 }, 800);
+  });
+})(window, window.jQuery);
 
 // Select all links with hashes
 $('div[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-        // On-page links
-        if (
-            location.pathname.replace(/^\//, '') ==
-            this.pathname.replace(/^\//, '') &&
-            location.hostname == this.hostname
-        ) {
-            // Figure out element to scroll to
-            var target = $(this.hash)
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
-                // Does a scroll target exist?
-            if (target.length) {
-                // Only prevent default if animation is actually gonna happen
-                event.preventDefault()
-                $('html, body').animate({
-                        scrollTop: target.offset().top,
-                    },
-                    1000,
-                    function() {
-                        // Callback after animation
-                        // Must change focus!
-                        var $target = $(target)
-                        $target.focus()
-                        if ($target.is(':focus')) {
-                            // Checking if the target was focused
-                            return false
-                        } else {
-                            $target.attr('tabindex', '-1') // Adding tabindex for elements not focusable
-                            $target.focus() // Set focus again
-                        }
-                    },
-                )
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
             }
-        }
-    })
+          }
+        );
+      }
+    }
+  });
